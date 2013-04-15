f5cd07b2fdee7c17acba9124be81ad11186fe477
olegrog <olegrog@73e5c0ec-c619-400c-a3d0-9f88a2d9b11f>
2013-04-15 15:27:22 +0000

    remove license
    git-svn-id: https://localhost/svn/openfoam@2 73e5c0ec-c619-400c-a3d0-9f88a2d9b11f

--- applications/heatEquationFoam/heatEquationFoam.C
+++ applications/heatEquationFoam/heatEquationFoam.C
@@ -5,27 +5,13 @@
     \\  /    A nd           | Copyright (C) 2011 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
-License
-    This file is part of OpenFOAM.
-
-    OpenFOAM is free software: you can redistribute it and/or modify it
-    under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
-    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-    for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
-
 Application
-    gas_heatFoam
+    heatEquationFoam
 
 Description
     Solves a heat equation for hard-sphere gas.
+    The thermal conductivity is proportional to the square root of the
+    temperature.
 
 \*---------------------------------------------------------------------------*/
 
@@ -48,8 +34,7 @@ int main(int argc, char *argv[])
     {
         Info<< "Time = " << runTime.timeName() << nl << endl;
 
-        solve
-        (
+        solve (
             fvm::laplacian (sqrt(T), T)
         );
     	runTime.write();
