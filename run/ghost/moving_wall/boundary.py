8d37f7ead775fd6c010b1b1e43172c93735dea62
olegrog <olegrog@73e5c0ec-c619-400c-a3d0-9f88a2d9b11f>
2013-05-07 08:42:36 +0000

    add Makefiles
    git-svn-id: https://localhost/svn/openfoam@6 73e5c0ec-c619-400c-a3d0-9f88a2d9b11f

---
+++ run/ghost/moving_wall/boundary.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import numpy as np
+alpha = .5
+beta  = 2./np.sqrt(np.pi)
+N = 100
+x = (np.arange(N)+.5)/N
+T = 1-alpha*np.cos(2*np.pi*x)
+dTdx = 2*np.pi*alpha*np.sin(2*np.pi*x)
+
+K1 = -0.6463
+u  = beta - K1*np.sqrt(T)*dTdx
+
+def print_T(arr):
+	print '\n'.join(['%f' % arr[i] for i in xrange(len(arr))])
+
+def print_u(arr):
+	print '\n'.join(['(%f 0 0)' % arr[i] for i in xrange(len(arr))])
+
+#print_T(T)
+#print '\n'
+print_u(u)
