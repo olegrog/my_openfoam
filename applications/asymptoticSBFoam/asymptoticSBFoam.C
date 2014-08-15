9ddb9216e5ef22961900caeea2e3fb7622fe59b1
Oleg Rogozin <o.a.rogozin@gmail.com>
2014-08-15 12:24:40 +0400

    [asymtoticSBFoam] some cleaning in the solver

--- applications/asymptoticSBFoam/asymptoticSBFoam.C
+++ applications/asymptoticSBFoam/asymptoticSBFoam.C
@@ -19,7 +19,6 @@ Description
 
 #include "fvCFD.H"
 #include "simpleControl.H"
-#include "fvIOoptionList.H"
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
@@ -28,11 +27,13 @@ int main(int argc, char *argv[])
     #include "setRootCase.H"
     #include "createTime.H"
     #include "createMesh.H"
-    #include "createFields.H"
-    #include "createFvOptions.H"
-    #include "initContinuityErrs.H"
 
     simpleControl simple(mesh);
+
+    #include "createFields.H"
+    #include "initContinuityErrs.H"
+
+    Info<< "\nStarting time loop\n" << endl;
 
     while (simple.loop())
     {
@@ -45,51 +46,45 @@ int main(int argc, char *argv[])
             fvm::laplacian((g2/2)*sqrt(T), T) == tr(DU)
         );
         TEqn.solve();
-        DT = fvc::grad(T);
 
         /** SIMPLE algorithm for solving pressure-velocity equations */
         
         /** velocity predictor */
-        volScalarField invT(1/T);
+        DT = fvc::grad(T);
         tmp<fvVectorMatrix> UEqn(
               fvm::div(phi, U)
-            - fvm::laplacian(g1*sqrt(T)/2, U)
-            + fvc::laplacian(g1*sqrt(T)/2, U)
-            - g1 * fvc::div(sqrt(T)*dev(symm(DU)))
-            - g7/T * (sqr(DT) & (U/g2/sqrt(T) - DT/4))
-	    == fvOptions(invT, U)
+            - fvm::laplacian(g1*sqrt(T)/2, U) ==
+              g1 * fvc::div(sqrt(T)*dev2(::T(DU)))
+            + g7/T * (sqr(DT) & (U/g2/sqrt(T) - DT/4))
         );
         UEqn().relax();
-	fvOptions.constrain(UEqn());
         solve(UEqn() == -fvc::grad(p));
-	fvOptions.correct(U);
 
         /** pressure corrector */
         volScalarField rAU(1./UEqn().A());
-	volVectorField HbyA("HbyA", U);
+        volVectorField HbyA("HbyA", U);
         HbyA = rAU*UEqn().H();
-	UEqn.clear();
+        UEqn.clear();
 
-	surfaceScalarField phiHbyA("phiHbyA", fvc::interpolate(HbyA/T) & mesh.Sf());
-        adjustPhi(phiHbyA, U, p);
-	fvOptions.makeRelative(phiHbyA);
+        surfaceScalarField phiHbyA("phiHbyA", fvc::interpolate(HbyA/T) & mesh.Sf());
+        bool closedVolume = adjustPhi(phiHbyA, U, p);
 
-	while (simple.correctNonOrthogonal()) {
+        while (simple.correctNonOrthogonal()) {
             fvScalarMatrix pEqn(
                 fvm::laplacian(rAU/T, p) == fvc::div(phiHbyA)
             );
-            pEqn.setReference(0, 0);
+            pEqn.setReference(pRefCell, pRefValue);
             pEqn.solve();
-            if (simple.finalNonOrthogonalIter())
-		phi = phiHbyA - pEqn.flux();
-	}
-	#include "continuityErrs.H"
+            if (simple.finalNonOrthogonalIter()) {
+                phi = phiHbyA - pEqn.flux();
+            }
+        }
+        #include "continuityErrs.H"
         p.relax();
-	
+
         /** velocity corrector */
         U = HbyA - rAU*fvc::grad(p);
         U.correctBoundaryConditions();
-	fvOptions.correct(U);
 
         runTime.write();
     }
