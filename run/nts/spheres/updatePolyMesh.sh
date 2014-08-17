8d94e971ac4f5421e35ff8cdfbbce9a75df47916
Oleg Rogozin <o.a.rogozin@gmail.com>
2014-08-18 00:02:41 +0400

    [spheres] update as the noncoaxial problem

---
+++ run/nts/spheres/updatePolyMesh.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+file="constant/polyMesh/boundary"
+old="$file.gmsh"
+mv $file $old
+
+sed '/physicalType/d' $old | awk '
+BEGIN {
+    type=""
+} 
+{
+    if ($1 == "back" || $1 == "front")  type="wedge";
+    if ($1 == "symmetry")               type="symmetryPlane";
+    if ($1 == "outer" || $1 == "inner") type="wall";
+    if ($1 == "type" && type) {
+        print "        type            "type";";
+        type="";
+    } else print;
+}' | sed '/\ttype/s/ //g' > $file
