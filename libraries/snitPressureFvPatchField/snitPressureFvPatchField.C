7f5b11da85aac3c7b0c10b76659e95cee2f163fa
Oleg Rogozin <o.a.rogozin@gmail.com>
2014-08-23 02:31:57 +0400

    add snitPressureFvPatchField

---
+++ libraries/snitPressureFvPatchField/snitPressureFvPatchField.C
@@ -0,0 +1,110 @@
+/*---------------------------------------------------------------------------*\
+| =========                 |                                                 |
+| \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox           |
+|  \\    /   O peration     | Version:  2.3.0                                 |
+|   \\  /    A nd           | Web:      www.OpenFOAM.org                      |
+|    \\/     M anipulation  |                                                 |
+\*---------------------------------------------------------------------------*/
+
+#include "snitPressureFvPatchField.H"
+#include "volFields.H"                      // for volScalarField
+#include "addToRunTimeSelectionTable.H"     // for makePatchTypeField
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+snitPressureFvPatchField::snitPressureFvPatchField
+(
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF
+)
+:
+    fixedGradientFvPatchScalarField(p, iF)
+{}
+
+
+snitPressureFvPatchField::snitPressureFvPatchField
+(
+    const snitPressureFvPatchField& ptf,
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF,
+    const fvPatchFieldMapper& mapper
+)
+:
+    fixedGradientFvPatchScalarField(ptf, p, iF, mapper)
+{}
+
+
+snitPressureFvPatchField::snitPressureFvPatchField
+(
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF,
+    const dictionary& dict
+)
+:
+    fixedGradientFvPatchScalarField(p, iF)
+{
+    // initialize as zero gradient, because transportProperties are inaccessible
+    operator==(patchInternalField());
+}
+
+
+snitPressureFvPatchField::snitPressureFvPatchField
+(
+    const snitPressureFvPatchField& ptf
+)
+:
+    fixedGradientFvPatchScalarField(ptf)
+{}
+
+
+snitPressureFvPatchField::snitPressureFvPatchField
+(
+    const snitPressureFvPatchField& ptf,
+    const DimensionedField<scalar, volMesh>& iF
+)
+:
+    fixedGradientFvPatchScalarField(ptf, iF)
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void snitPressureFvPatchField::updateCoeffs()
+{
+    if (updated())
+    {
+        return;
+    }
+
+    const dictionary& transportProperties =
+        db().lookupObject<IOdictionary>("transportProperties");
+    const scalar gamma7 =
+        dimensionedScalar(transportProperties.lookup("gamma7")).value();
+    const fvPatchField<scalar>& T =
+        patch().lookupPatchField<volScalarField, scalar>("T");
+
+    gradient() = -0.5 * gamma7 / T * pow3(T.snGrad());
+    fixedGradientFvPatchScalarField::updateCoeffs();
+}
+
+void snitPressureFvPatchField::write(Ostream& os) const
+{
+    fvPatchScalarField::write(os);
+}
+
+makePatchTypeField
+(
+    fvPatchScalarField,
+    snitPressureFvPatchField
+);
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Foam
+
+// ************************************************************************* //
