07b6c77e73bc6d7195c4b519790a9790969854c9
Oleg Rogozin <o.a.rogozin@gmail.com>
2014-09-18 22:14:06 +0400

    [wallSnitForces] fix critical bug + new names

--- applications/wallSnitForces/wallSnitForces.C
+++ applications/wallSnitForces/wallSnitForces.C
@@ -60,67 +60,22 @@ int main(int argc, char *argv[])
         #include "createFields.H"
 
         // NB: mesh.Sf() is a normal, pointed out of the gas on the boundary
-        const surfaceVectorField pressure0(
-            fvc::interpolate(2 * p) * mesh.Sf()
+
+        const surfaceVectorField hydrostatic(
+            2 * fvc::interpolate(p) * mesh.Sf()
         );
-        // we avoid laplacian scheme for benefit of more precise gradient one
-        const surfaceVectorField pressure3(
-            fvc::interpolate(
-                //-2 * g3 / 3 * fvc::laplacian(T, T)
-                /**/
-                - g3 / 3 * (
-                    magSqr(fvc::grad(T)) + 4 * (U & fvc::grad(T)) / (g2 * sqrt(T))
-                )
-                /**/
-            ) * mesh.Sf()
+        const surfaceVectorField viscous(
+            g1*fvc::interpolate(sqrt(T)) * (
+                - 5/6 * fvc::interpolate(fvc::div(U)) * mesh.Sf()
+                - fvc::snGrad(U) * mesh.magSf()
+            )
         );
-        const surfaceVectorField pressure7(
-            fvc::interpolate(
-                g7 / 6 * magSqr(fvc::grad(T))
-            ) * mesh.Sf()
-        );
-        const surfaceVectorField shearStress1(
-            //- g1 * (mesh.Sf() & fvc::interpolate(sqrt(T) * fvc::grad(U)))
-            //- g1 * fvc::interpolate(sqrt(T)) * fvc::snGrad(U) * mesh.magSf()
-            /**/
-            twoSymm(dev(fvc::interpolate(
-                -g1 * sqrt(T) * fvc::grad(U)
-            ))) & mesh.Sf()
-            /**/
-        );
-        const surfaceVectorField shearStress3(
-            //g3 * fvc::snGrad(T) * (fvc::interpolate(T * curvature) - fvc::snGrad(T) / 3) * mesh.Sf()
-            /**/
-            // the second derivative of T can be asymmetric due to interpolation
-            /*
-            symm(dev(fvc::interpolate(
-                g3 * T * fvc::grad(fvc::grad(T))
-            ))) & mesh.Sf()
-            */
-            /*
-            fvc::interpolate(
-                -2 *g3* T * fvc::grad(fvc::grad(T))
-            ) & mesh.Sf()
-            */
-            -2*g3*fvc::interpolate(T*fvc::div(magSqr(mesh.Sf()), fvc::grad(T)))
-            /**/
-        );
-        const surfaceVectorField shearStress7(
-            //2 * g7 / 3 * sqr(fvc::snGrad(T)) * mesh.Sf()
-            /**/
-            /*
-            dev(fvc::interpolate(
-                g7 * sqr(fvc::grad(T))
-            )) & mesh.Sf()
-            */
-            fvc::interpolate(
-                g3*magSqr(fvc::grad(T))
-            ) * mesh.Sf()
-            /**/
+        const surfaceVectorField thermal(
+            - g7 * fvc::interpolate(fvc::grad(T)) * fvc::snGrad(T) * mesh.magSf()
+            + g7/2 * magSqr(fvc::interpolate(fvc::grad(T))) * mesh.Sf()
         );
         const surfaceVectorField force(
-            pressure0 + pressure3 + pressure7 +
-            shearStress1 + shearStress3 + shearStress7
+            hydrostatic + viscous + thermal
         );
         const surfaceVectorField moment(
             mesh.Cf() ^ force
@@ -133,27 +88,20 @@ int main(int argc, char *argv[])
             {
                 Info<< mesh.boundary()[patchi].name()
                     << " " << gSum(force.boundaryField()[patchi])
-                    << " consisted of\n\tshear stresses: "
-                    << "gamma_1 " << gSum(shearStress1.boundaryField()[patchi])
-                    << ", gamma_3 " << gSum(shearStress3.boundaryField()[patchi])
-                    << ", gamma_7 " << gSum(shearStress7.boundaryField()[patchi])
-                    << ",\n\tand pressures: "
-                    << "p^dag " << gSum(pressure0.boundaryField()[patchi])
-                    << ", gamma_3 " << gSum(pressure3.boundaryField()[patchi])
-                    << ", gamma_7 " << gSum(pressure7.boundaryField()[patchi])
-                    << ".\n\tmoment of force relative to origin: "
+                    << " consisted of:\n"
+                    << "\thydrostatic " << gSum(hydrostatic.boundaryField()[patchi])
+                    << ", viscous " << gSum(viscous.boundaryField()[patchi])
+                    << ", thermal " << gSum(thermal.boundaryField()[patchi])
+                    << ",\n\tmoment of force relative to origin: "
                     << gSum(moment.boundaryField()[patchi])
-                    << endl;
+                    << "." << endl;
             }
         }
         Info<< endl;
 
-        writeWallField("Pressure0", pressure0, mesh, runTime);
-        writeWallField("Pressure3", pressure3, mesh, runTime);
-        writeWallField("Pressure7", pressure7, mesh, runTime);
-        writeWallField("ShearStress1", shearStress1, mesh, runTime);
-        writeWallField("ShearStress3", shearStress3, mesh, runTime);
-        writeWallField("ShearStress7", shearStress7, mesh, runTime);
+        writeWallField("HydrostaticForce", hydrostatic, mesh, runTime);
+        writeWallField("ViscousForce", viscous, mesh, runTime);
+        writeWallField("ThermalForce", thermal, mesh, runTime);
         writeWallField("Force", force, mesh, runTime);
         writeWallField("Moment", moment, mesh, runTime);
 
