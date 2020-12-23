f661731438147d81aea5bac9d7e5c8ca16e57a2a
Oleg Rogozin <o.a.rogozin@gmail.com>
2020-12-24 01:13:38 +0300

    [tools] update ParaView scripts to python3

--- tools/paraview/R.py
+++ tools/paraview/R.py
@@ -3,7 +3,7 @@
 from paraview.simple import *
 
 # Find the original source and reload it
-for key, value in GetSources().iteritems():
+for key, value in GetSources().items():
     if key[0].endswith('foam'):
         print("Reload " + key[0]);
         ReloadFiles(value[0])
