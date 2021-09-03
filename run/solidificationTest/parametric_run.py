25dabb5982ee035f0b5c135160176644ad53c93b
Oleg Rogozin <o.a.rogozin@gmail.com>
2021-09-03 17:04:13 +0300

    [solidificationTest] improve parametric_run.py
    1. Extended and colorized information in the progress mode
    2. Correct endTime & writeInterval according to the pulling speed
    3. Do not fail if WCOLL is absent
    4. Fix bug with stopping ffmpeg in background (Allrun)

--- run/solidificationTest/parametric_run.py
+++ run/solidificationTest/parametric_run.py
@@ -8,23 +8,32 @@ str2list = lambda s: [float(item) for item in s.split(',')]
 parser = argparse.ArgumentParser(description='Script for running parametric simulations')
 parser.add_argument('-G', type=str2list, default='5e5,1e6,2e6', help='delimited list of temperature gradients')
 parser.add_argument('-V', type=str2list, default='0.012,0.03', help='delimited list of pulling speeds')
-parser.add_argument('-f', '--hostfile', type=str, default=os.environ['WCOLL'], help='path to the hostfile')
+parser.add_argument('-f', '--hostfile', type=str, default=None, help='path to the hostfile')
 parser.add_argument('-s', '--scale', type=float, default=2, help='rescale all dimensions')
 parser.add_argument('-y', '--yscale', type=float, default=2, help='rescale along the temperature gradient')
 parser.add_argument('-W', '--Wscale', type=float, default=0.5, help='rescale the interface width')
+parser.add_argument('-t', '--tscale', type=float, default=1.5, help='rescale the simulation time')
+parser.add_argument('-N', '--Nwrites', type=int, default=100, help='number of writings during L/V')
 parser.add_argument('--prefix', type=str, default='_case', help='prefix for directory names')
 parser.add_argument('-p', '--progress', action='store_true', help='print progress instead of running')
 parser.add_argument('-n', '--dry-run', action='store_true', help='do not launch simulations')
 parser.add_argument('-v', '--verbose', action='store_true', help='increase output verbosity')
 args = parser.parse_args()
 
-if args.verbose:
+if not args.hostfile:
+    hostvar = 'WCOLL'
+    print(f'Option --hostfile is missed. Trying to use {hostvar} variable...')
+    args.hostfile = os.environ[hostvar]
+if args.verbose or args.progress:
     from termcolor import colored
+if args.progress:
+    import time, humanize
 
 class Regex:
     float1 = re.compile(r'-?[0-9]+\.?[0-9]*[Ee]?[+-]?[0-9]*')
     int3 = re.compile(r'\([-\d.]+ +[-\d.]+ +[-\d.]+\)')
     time = re.compile(r'^Time = ([0-9]+\.?[0-9]*[Ee]?[+-]?[0-9]*)')
+    deltaT = re.compile(r'^deltaT = ([0-9]+\.?[0-9]*[Ee]?[+-]?[0-9]*)')
 
 def reverse_readline(filename, buf_size=8192):
     """A generator that returns the lines of a file in reverse order
@@ -90,8 +99,8 @@ def overwrite_property(filename, name, func):
     value = float(read_property(filename, name))
     change_property(filename, name, f'{func(value)}')
 
-def change_blockMesh(filename):
-    ivert = 0
+def change_mesh(filename):
+    Lmin = Lmax = ivert = 0
     with fileinput.input(filename, inplace=True) as f:
         for line in f:
             if 'vertices' in line:
@@ -101,6 +110,8 @@ def change_blockMesh(filename):
                 func = lambda line: ' '.join(map(lambda x: str(float(x[1])*args.yscale) \
                     if x[0] == 1 else x[1], enumerate(line.split()))) + '\n'
                 line = overwrite_line(line, func)
+                y = float(line.split()[1])
+                Lmin, Lmax = min(Lmin, y), max(Lmax, y)
             if 'simpleGrading' in line:
                 cells = Regex.int3.findall(line)[0].split()
                 cellsX = int(int(cells[0].replace('(', ''))*args.scale/args.Wscale)
@@ -109,21 +120,52 @@ def change_blockMesh(filename):
                 line2[9] = f'({cellsX}'; line2[10] = f'{cellsY}'
                 line = overwrite_line(line, lambda _: ' '.join(line2) + '\n')
             print(line, end='')
+    return Lmax - Lmin
 
 app = read_property('system/controlDict', 'application')
 cwd = os.getcwd()
 
 if args.progress:
-    print(f'{"Case":30s}Progress')
+    print(f'{"Case":30s}{"Vp":7s}{"deltaT":9s}{"Progress":14s}{"Elapsed time":20s}{"Finish time":20s}')
+    now = time.time()
     for case in glob.glob(f'{args.prefix}*'):
-        end_time = float(read_property(os.path.join(case, 'system/controlDict'), 'endTime'))
+        problemProperties = os.path.join(case, 'constant/problemProperties')
+        controlDict = os.path.join(case, 'system/controlDict')
+        videofile = os.path.join(case, 'video.avi')
         logfile = os.path.join(case, f'log.{app}')
+
+        G = float(read_property(problemProperties, 'tempGradient'))
+        dotT = float(read_property(problemProperties, 'coolingRate'))
+        Vp = dotT/G
+        end_time = float(read_property(controlDict, 'endTime'))
         if os.path.exists(logfile):
             curr_time = float(read_last_regex(logfile, Regex.time))
-            progress = f'{int(curr_time*100/end_time)}%'
+            deltaT = f'{float(read_last_regex(logfile, Regex.deltaT)):.1e}'
+            progress = curr_time/end_time
+            time_since_last_writing = now - os.path.getmtime(logfile)
+            elapsed_time = os.path.getmtime(logfile) - os.path.getmtime(problemProperties)
+            if progress < 1:
+                remaining_time = elapsed_time/progress*(1 - progress)
+                finish_time = humanize.naturaltime(remaining_time, future=True)
+            else:
+                finish_time = humanize.naturaltime(time_since_last_writing)
+            progress = f'{progress:.0%}'
+            color = 'yellow'
+            if time_since_last_writing > 30:
+                finish_time = 'stopped ' + humanize.naturaltime(time_since_last_writing)
+                color = 'red'
         else:
-            progress = 'not running'
-        print(f'{case:30s}{progress}')
+            deltaT = ''
+            elapsed_time = ''
+            finish_time = ''
+            progress = 'not started'
+            color = 'red'
+        if os.path.exists(videofile):
+            finish_time = humanize.naturaltime(now - os.path.getmtime(videofile))
+            progress += ' + video'
+            color = 'green'
+        elapsed_time = humanize.precisedelta(elapsed_time, minimum_unit='hours', format='%.0f')
+        print(colored(f'{case:30s}{str(Vp):7s}{deltaT:9s}{progress:14s}{elapsed_time:20s}{finish_time:20s}', color))
     sys.exit()
 
 if not args.dry_run:
@@ -164,10 +206,16 @@ for i, G in enumerate(args.G):
 
             f = os.path.join(case, 'system/blockMeshDict')
             overwrite_property(f, 'scale', lambda x: x*args.scale)
-            change_blockMesh(f)
+            scale = float(read_property(f, 'scale'))
+            L = change_mesh(f)
+            total_time = L*scale/V
+
+            f = os.path.join(case, 'system/controlDict')
+            overwrite_property(f, 'endTime', lambda x: total_time*args.tscale)
+            overwrite_property(f, 'writeInterval', lambda x: total_time/args.Nwrites/args.yscale)
 
         if os.path.exists(os.path.join(case, f'log.{app}')):
-            print(f' -- Case {case} is already simulated')
+            print(f' -- Directory {case} contains simulation results')
             continue
         if args.dry_run:
             continue
