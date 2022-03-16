0ec27d2ff445ac93e610539f69e5f9f78a665d6a
Oleg Rogozin <o.a.rogozin@gmail.com>
2022-03-16 20:45:55 +0300

    [singleTrackPrinting] fix generation of vtk.series in parallel case

--- tools/obj2vtkseries.py
+++ tools/obj2vtkseries.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 
-import argparse, os, re, json
+import argparse, os, sys, re, json
 
 parser = argparse.ArgumentParser(description='Convert OBJ to legacy VTK series')
 parser.add_argument('dir', help='directory where OBJ files are placed')
 args = parser.parse_args()
 
-times = [ float(f) for f in os.listdir('.') if re.match(r'[0-9]+[0-9.e+-]*', f) ]
-times.sort()
-del times[0]
+for tdir in ['.', 'processor0']:
+    times = [ float(f) for f in os.listdir(tdir) if re.match(r'[0-9]+[0-9.e+-]*', f) ]
+    times.sort()
+    del times[0]
+    if len(times):
+        break
+
+if not len(times):
+    print('Empty list of times!', file=sys.stderr)
 
 files = [ f[:-3] + 'vtk' for f in os.listdir(args.dir) if re.match(r'.*\.obj', f) ]
 
